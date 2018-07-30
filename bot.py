ccc4695117369e1dc2538c2f9b8527e2edbc5bed
Lakshya Ranwan <lakshyaranwan4@gmail.com>
2018-07-30 22:36:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import time
 from numpy import *
 
 class cordinates:
+    #the coordinates of the replay button of the game, depending on your screen size
     replayBtn = (360,240)
 
 def restartGame():
@@ -15,6 +16,8 @@ def jump():
     pyautogui.keyUp('space')
 
 def imageGrab():
+    #the coordinates of a square in front of the dino, when a tree enters the box, the dino jumps
+    #x1,y1 top left corner ; x2,y2 bottom right corner
     box = (150,242,250,274)
     image = ImageGrab.grab(box)
     greyImage = ImageOps.grayscale(image)
@@ -24,6 +27,7 @@ def imageGrab():
 restartGame()
 while True:
     print(imageGrab())
+    #3447 is the value of the sum of the pixels greyscale values of the box when there is no tree
     if(imageGrab()!=3447):
         jump()
 
